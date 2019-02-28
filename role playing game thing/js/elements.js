118448219b3e9eebe2659048083d3d959b708419
HugoHead <45836267+HugoHead@users.noreply.github.com>
2019-02-27 21:45:15 -0500

    Added subfolders
    You are welcome Dane.

---
+++ role playing game thing/js/elements.js
@@ -0,0 +1,20 @@
+function makeElements()
+{
+	//componet(width, height, x, y, colorOrURL, parant, classes, id)
+	var terain1 = new componet(384, 192, 700, 99, "green", $("#game_elements"), ["darn_obstacle"]);
+	var terain2 = new componet(387, 400, 1150, 200, "green", $("#game_elements"), ["darn_obstacle"]);
+	var terain3 = new componet(367, 168, 333, 373, "green", $("#game_elements"), ["darn_obstacle"]);
+	var terain4 = new componet(384, 168, 700, 373, "green", $("#game_elements"), ["darn_obstacle"]);
+	var terain5 = new componet(313, 190, 333, 245, "green", $("#game_elements"), ["darn_obstacle"]);
+	var terain6 = new componet(384, 192, 404, 732, "green", $("#game_elements"), ["darn_obstacle"]);
+	var terain7 = new componet(387, 192, 1150, 545, "green", $("#game_elements"), ["darn_obstacle"]);
+	var terain8 = new componet(384, 192, 766, 595, "green", $("#game_elements"), ["darn_obstacle"]);
+	var terain9 = new componet(384, 195, 174, 540, "green", $("#game_elements"), ["darn_obstacle"]);
+	var terain10 = new componet(384, 192, 337, -6, "green", $("#game_elements"), ["darn_obstacle"]);
+    var topBar = new componet(1074, 192, 10, -250, "green", $("#game_elements"), ["darn_obstacle", "topBar"]);
+    var bottomBar = new componet(394, 10, 20, 914, "green", $("#game_elements"), ["darn_obstacle"]);
+    var barThatPreventsEscapefromTheMerchantArea = new componet(192, 402, 1074, -269, "green", $("#game_elements"), ["darn_obstacle"])    ;
+	var cave1 = new componet(635, 198, 449, 539, "green", $("#game_elements"), ["cave"]);
+	var cave2 = new componet(363, 198, 721, -6, "green", $("#game_elements"), ["cave"]);
+    return [terain1,terain2,terain3,terain4,terain5,terain6,terain7,terain8,terain9,terain10,topBar,barThatPreventsEscapefromTheMerchantArea,cave1,cave2];
+}
---
+++ role playing game thing/js/objectsFactory.js
@@ -0,0 +1,137 @@
+function componet(width, height, x, y, colorOrURL, parant, classes, id)
+{
+    this.width = width.toString(10) + "px";
+    this.height = height.toString(10) + "px";
+    this.x = x.toString(10) + "px";
+    this.y = y.toString(10) + "px";
+    this.colorOrURL = colorOrURL;
+    this.classes = classes;
+    this.parant = parant;
+    this.element;this.id;
+    this.update = function()
+    {
+        this.element = document.createElement("DIV");
+        this.element.style.width = this.width;
+        this.element.style.height = this.height;
+        this.element.style.top = this.y;
+        this.element.style.left = this.x;
+        if (this.classes != undefined)
+        {
+            var length = classes.length;
+            for (var i = 0; i < length; i++)
+            {
+                this.element.classList.add(classes[i]);
+            }
+        }
+        if (this.id != undefined)
+        {
+            this.element.setAttribute("id", this.id);
+        }
+        this.parant.append(this.element);
+        if (this.colorOrURL.search("."))//the colorOrUrl is a url
+        {
+            this.element.style.backgroundImage = colorOrURL;
+        }
+        else
+        {
+            this.element.style.backgroundColor = this.colorOrURL;
+        }
+    }
+    this.update();
+}
+//------------------------------------------------------------------------
+//------------------------------------------------------------------------
+function entity (width, height, x, y, colorOrURL, classes, type, health)
+{
+    this.width = width.toString(10) + "px";
+    this.height = height.toString(10) + "px";
+    this.x = x.toString(10) + "px";
+    this.y = y.toString(10) + "px";
+    this.colorOrURL = colorOrURL;
+    this.type = type;
+    this.element;this.health;
+    this.orintation;
+    this.update = function()
+    {
+        this.element = document.createElement("DIV");
+        this.element.style.width = this.width;
+        this.element.style.height = this.height;
+        this.element.style.top = this.y;
+        this.element.style.left = this.x;
+        if (this.classes != undefined)
+        {
+            var length = classes.length;
+            for (var i = 0; i < length; i++)
+            {
+                this.element.classList.add(classes[i]);
+            }
+        }
+        if (this.id != undefined)
+        {
+            this.element.setAttribute("id", this.id);
+        }
+        if (this.colorOrURL.search("."))//the colorOrUrl is a url
+        {
+            this.element.style.backgroundImage = colorOrURL;
+        }
+        else
+        {
+            this.element.style.backgroundColor = this.colorOrURL;
+        }
+    }
+    this.spawn = function()
+    {
+         body.append(this.element);
+    }
+    this.move = function(rotarr, distarr, speed)
+    {
+        var rotDist = rotarr[0],
+        rotRate = rotarr[1],
+        dist = distarr[0],
+        rate = distarr[1];
+
+        const reps = rotarr.length;
+        var currentVals = get_aspects(this.element);
+        var currentLeft = currentVals.left;
+        var currentTop = currentVals.top;
+        //upate the orientatoin property
+        this.orintation = parseInt(this.element.style.transform.replace("rotate(", "").replace("deg)", ""));
+
+        var rotAmm = rotarr.length;
+        var distAmm = distarr.lenght;
+
+        for (var r = 0; r < reps; r++)
+        {
+              //execute rotarr[r]
+              this.orintation += rotarr[r];
+              this.element.style.transform = "rotate(" + this.orintation % 360 + "deg)";
+
+              //execute disarr[r]
+              var xdist = Math.cos(this.orintation) * distarr[r];
+              var ydist = Math.sin(this.orintation) * distarr[r];
+              this.element.animate({
+                  left: "+=" + xdist,
+                  top: "+=" + ydist
+              }, speed, function() {
+                    clog("Animation complete.");
+              });
+
+        }
+        if (distAmm != rotAmm)//the number of linear motions requested is greater than the number of
+        {
+            //execute distarr[distAmm]
+        }
+    }
+    this.update();
+    /*
+    *methods needed:
+    *Die
+    *Change image/color
+    *move (inpendent of player/toards player)
+    *input/output damage
+    *check for death
+    *Attack
+    *spawn
+    *
+    */
+}
